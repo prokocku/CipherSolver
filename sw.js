212167f5f0a8837d1671b28ffa6e998192255417
prokocku <prokocku@gmail.com>
2026-06-14 15:35:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker pro Šifry PWA
 // Verze cache — změň číslo při každém nasazení nové verze
-const CACHE = 'sifry-v13.0';
+const CACHE = 'sifry-v13.1';
 
 const ASSETS = [
   './index.html',
